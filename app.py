f3f496ad95b1f00e2dc041c87e481fa03b93d0d8
Ubuntu <ubuntu@ip-172-31-90-153.ec2.internal>
2019-08-23 11:16:19 +0000

    attempted bug fix for Materialise M object- not loading- revisit

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def update_task(task_id):
         {
         'task_name': request.form.get('task_name'),
         'category_name': request.form.get('category_name'),
-        'task.descripton': request.form.get('task_description'),
+        'task_description': request.form.get('task_description'),
         'due_date': request.form.get('due_date'),
         'is_urgent': request.form.get('is_urgent')
         }
